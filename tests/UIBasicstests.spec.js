a3c3c25ada309a0e16cea240c67cafa8698c73ce
punamthapa <punam.chhetri.372@gmail.com>
2025-05-23 19:40:02 +0100

    making constant and reusing for login

--- tests/UIBasicstests.spec.js
+++ tests/UIBasicstests.spec.js
@@ -1,17 +1,23 @@
 import { test, expect } from '@playwright/test';
 
-test('first Playwright Test', async ({  page}) => {
+test('test for incorrect username and password', async ({  page}) => {
+    const userName= page.locator("#username");
+    const password= page.locator("#password");
+    const loginButton = page.locator("#signInBtn");
+
     
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     console.log(await page.title());
-    await page.locator("#username").fill("learning")
-    await page.locator("#password").fill("learning")
+    await userName.fill("learning")
+    await page.locator("#password").fill("tets")
     await page.locator("#signInBtn").click()  ;
     console.log(await page.locator(" [style*='block']").textContent());
-    await expect(page.locator("[style*='block']")).toContainText("Incorrect");
-    
-    
-   
+    await expect(page.locator("[style*='display']")).toContainText("Incorrect username/password.");
+
+    await userName.fill("")
+    await userName.fill("rahulshettyacademy");
+    await password.fill("learning");
+    await loginButton.click();  
     
   });
   
