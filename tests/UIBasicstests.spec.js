0ab7a17d7d07ed0839ed9a116f26b23906f78bb7
punamthapa <punam.chhetri.372@gmail.com>
2025-05-23 20:05:44 +0100

    all text

--- tests/UIBasicstests.spec.js
+++ tests/UIBasicstests.spec.js
@@ -4,6 +4,7 @@ test('test for incorrect username and password', async ({  page}) => {
     const userName= page.locator("#username");
     const password= page.locator("#password");
     const loginButton = page.locator("#signInBtn");
+    const cardTitles = page.locator(".card-body a")
 
     
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
@@ -17,7 +18,16 @@ test('test for incorrect username and password', async ({  page}) => {
     await userName.fill("")
     await userName.fill("rahulshettyacademy");
     await password.fill("learning");
-    await loginButton.click();  
+    await loginButton.click();
+    
+   // await page.locator(".card-body a ").nth(0).textContent();
+   //dyanamic wait until all the network are loaded
+    await page.waitForLoadState("networkidle");
+    await cardTitles.first().waitFor();
+
+    const allTitles = await cardTitles.allTextContents();
+   
+  console.log(allTitles);
     
   });
   
